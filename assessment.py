3402cc6ef5059a631e3a88c347f349fd339bd62b
Wiezel77 <56902128+Wiezel77@users.noreply.github.com>
2022-05-23 17:19:51 -0400

    attributes of repos are put into a list

--- assessment.py
+++ assessment.py
@@ -1,3 +1,16 @@
 #Drew Wiesen - Tackle take home assessment
 
 from github import Github
+
+#Taken from docs to set up an instance of Github
+g = Github("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")
+
+user = g.get_user("drewwiesen155")
+repos = user.get_repos()
+
+repo_attrib = [];
+
+for repo in repos:
+    repo_attrib.append([repo.allow_merge_commit, repo.allow_squash_merge, repo.allow_rebase_merge])
+	
+#print(repo_attrib);
