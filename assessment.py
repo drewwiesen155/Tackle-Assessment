7c43c79572359b219d010b201fa47a404906ccda
Wiezel77 <56902128+Wiezel77@users.noreply.github.com>
2022-05-23 18:19:21 -0400

    grabbed repo data and outputs a JSON

--- assessment.py
+++ assessment.py
@@ -1,16 +1,31 @@
 #Drew Wiesen - Tackle take home assessment
 
 from github import Github
+import json
 
 #Taken from docs to set up an instance of Github
-g = Github("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")
+g = Github("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")
 
 user = g.get_user("drewwiesen155")
 repos = user.get_repos()
 
 repo_attrib = [];
 
+#loop that places all audit data into a repo list
 for repo in repos:
-    repo_attrib.append([repo.allow_merge_commit, repo.allow_squash_merge, repo.allow_rebase_merge])
+	temp = {
+		"name: ": repo.name,
+		"merge commit allowed: ": repo.allow_merge_commit,
+		"squash merge allowed: ": repo.allow_squash_merge,
+		"rebase merge allowed: ": repo.allow_rebase_merge,
+		"auto delete head branch": repo.delete_branch_on_merge
+		}
+	repo_attrib.append(temp)
 	
-#print(repo_attrib);
+#print(repo_attrib); for debugging
+
+#finalize as a json file
+
+
+with open("result.json", "w") as ret_file:
+	json.dump(repo_attrib, ret_file)
